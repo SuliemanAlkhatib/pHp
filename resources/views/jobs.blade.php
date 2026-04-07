7ee55f57d8f05a978d238a566580e778ace4db5b
SuliemanAlkhatib <sullllimaa963@gmail.com>
2026-04-07 15:51:10 +0300

    Refactor jobs view to use list items for job listings and update route data structure

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,7 +2,10 @@
       <h1>Jobs</h1>
     <x-slot:head>placeeeeeeeeeeeees</x-slot:head>
     <x-slot:feet>{{ $greeting }}</x-slot:feet>
+    <ul>
 @foreach ($jobs as $job)
-    <p><strong>{{ $job['title'] }}</strong> - ${{ number_format($job['salary']) }}</p>
+<a href="/jobs/{{ $job['id'] }}"><li><strong>{{ $job['title'] }}</strong> - ${{ number_format($job['salary']) }}</li></a>
+    
 @endforeach
+</ul>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,31 +12,37 @@ Route::get('/contacts',function (){
 });
 
 Route::get('/jobs',function (){
-
-    return view('jobs',[
-        'greeting' => 'Hello World',
-        'jobs'=>[
-            [
-                'title'=>'Laravel Developer',
-                'salary'=> 40000
-            ],
+                
+                return view('jobs',[
+                    'greeting' => 'Hello World',
+                    'jobs'=>[
+                        [
+                            'title'=>'Laravel Developer',
+                            'salary'=> 40000
+                            ],
             [
                 'title'=>'Vue Developer',
                 'salary'=> 35000
-            ],
-            [
-                'title'=>'React Developer',
-                'salary'=> 45000
+                ],
+                [
+                    'title'=>'React Developer',
+                    'salary'=> 45000
             ],
              [
                 'title'=>'Angular Developer',
                 'salary'=> 30000
-            ],
+                ],
              [
-                'title'=>'Node Developer',
+                 'title'=>'Node Developer',
                 'salary'=> 50000
-        ]
+                ]
 
     ]]);
+    });
+    
+Route::get('/jobs/{id}',function ($id){
+       dd($id);
+        return view('jobs');    
 });
 
+    

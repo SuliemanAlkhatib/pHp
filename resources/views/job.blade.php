b825129f2b4549578c6e064c0cd698fba3c4d5cf
SuliemanAlkhatib <sullllimaa963@gmail.com>
2026-04-07 18:24:04 +0300

    Add job details to job route and create job view template

---
+++ resources/views/job.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+      <h1>Job</h1>
+    <x-slot:head>{{ $job['title'] }}</x-slot:head>
+    
+    
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,38 @@ Route::get('/jobs',function (){
     });
     
 Route::get('/jobs/{id}',function ($id){
-       dd($id);
-        return view('jobs');    
+       $jobs=[
+                        ['id'=>1,
+                            'title'=>'Laravel Developer',
+                            'salary'=> 40000
+                            ],
+            [
+                'id'=>2,
+
+                'title'=>'Vue Developer',
+                'salary'=> 35000
+                ],
+                [
+                    'id'=>3,
+                    'title'=>'React Developer',
+                    'salary'=> 45000
+            ],
+             [
+                'id'=>4,
+                'title'=>'Angular Developer',
+                'salary'=> 30000
+                ],
+             [
+                 'id'=>5,
+
+                 'title'=>'Node Developer',
+                'salary'=> 50000
+                ]
+
+    ];
+
+   $job =  Arr::first($jobs, fn($job) => $job['id'] == $id);
+        return view('job',['job'=>$job] );    
 });
 
     
